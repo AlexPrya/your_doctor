cb3876c97f0e85597491b076a800d2fb8bece0d4
AlexPrya <pruashnikov@gmail.com>
2017-12-01 01:49:09 +0300

    main page, services

--- index.php
+++ index.php
@@ -1,5 +1,121 @@
-<?require("template/header.html");
+<?require("template/header.html");?>
 
+    <div class="main-services">
+        <div class="main-services-title">
+            <div class="main-services-title-bg-line"></div>
+            <div class="main-services-title-bg-delimiter-outer">
+                <div class="main-services-title-bg-delimiter-inner">
+                    <h2 class="main-services-title-text">Услуги клиники</h2>
+                </div>
+            </div>
+        </div>
 
+        <div class="main-services-list">
+            <div class="container main-services-items">
+                <div class="row main-services-row">
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="row main-services-row">
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="row main-services-row">
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                    <div class="col-md-3 main-services-item">
+                        <a href="#" class="main-services-item-link">
+                            <div class="main-services-item-img" style="background-image: url('template/img/diagnostika.svg')"></div>
+                            <span class="main-services-item-name">
+                                Диагностика
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-require("template/footer.html");?>
+<?require("template/footer.html");?>
