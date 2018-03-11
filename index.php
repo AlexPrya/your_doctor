4a0a5c660ca22fb1dd72e1ff2eba738da005f7b2
AlexPrya <pruashnikov@gmail.com>
2018-03-12 00:07:17 +0300

    main page, full

--- index.php
+++ index.php
@@ -117,5 +117,154 @@
             </div>
         </div>
     </div>
+    <div class="main-slider">
+        <div class="main-slider-back"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-6">
+                    <div class="main-slider-container">
+                        <ul class="nav nav-tabs nav-justified main-slider-nav">
+                            <li class="active main-slider-nav-item">
+                                <a href="#news" data-toggle="tab">
+                                    Новости
+                                </a>
+                            </li>
+                            <li class="main-slider-nav-item">
+                                <a href="#workers" data-toggle="tab">
+                                    Доктора
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="tab-content main-slider-body">
+                            <div class="tab-pane active" id="news">
+                                <div class="main-slider-carousel">
+                                    <div class="item">
+                                        <img src="template/img/news1.png" alt="">
+                                        <div class="carousel-caption">
+                                            <h3>Отбеливание со<br>скидкой 20%</h3>
+                                            <p>С белоснежной улыбкой в отпуск!</p>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="template/img/news1.png" alt="">
+                                        <div class="carousel-caption">
+                                            <h3>Отбеливание со<br>скидкой 20%</h3>
+                                            <p>С белоснежной улыбкой в отпуск!</p>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="template/img/news1.png" alt="">
+                                        <div class="carousel-caption">
+                                            <h3>Отбеливание со<br>скидкой 20%</h3>
+                                            <p>С белоснежной улыбкой в отпуск!</p>
+                                        </div>
+                                    </div>
+                                    <div class="item active">
+                                        <img src="template/img/news1.png" alt="">
+                                        <div class="carousel-caption">
+                                            <h3>Отбеливание со<br>скидкой 20%</h3>
+                                            <p>С белоснежной улыбкой в отпуск!</p>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="template/img/news1.png" alt="">
+                                        <div class="carousel-caption">
+                                            <h3>Отбеливание со<br>скидкой 20%</h3>
+                                            <p>С белоснежной улыбкой в отпуск!</p>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="template/img/news1.png" alt="">
+                                        <div class="carousel-caption">
+                                            <h3>Отбеливание со<br>скидкой 20%</h3>
+                                            <p>С белоснежной улыбкой в отпуск!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="workers">
+                                <div class="tab-pane" id="workers">
+                                    <div class="main-slider-carousel">
+                                        <div class="item">
+                                            <img src="template/img/news1.png" alt="">
+                                            <div class="carousel-caption">
+                                                <h3>Отбеливание со<br>скидкой 20%</h3>
+                                                <p>С белоснежной улыбкой в отпуск!</p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <img src="template/img/news1.png" alt="">
+                                            <div class="carousel-caption">
+                                                <h3>Отбеливание со<br>скидкой 20%</h3>
+                                                <p>С белоснежной улыбкой в отпуск!</p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <img src="template/img/news1.png" alt="">
+                                            <div class="carousel-caption">
+                                                <h3>Отбеливание со<br>скидкой 20%</h3>
+                                                <p>С белоснежной улыбкой в отпуск!</p>
+                                            </div>
+                                        </div>
+                                        <div class="item active">
+                                            <img src="template/img/news1.png" alt="">
+                                            <div class="carousel-caption">
+                                                <h3>Отбеливание со<br>скидкой 20%</h3>
+                                                <p>С белоснежной улыбкой в отпуск!</p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <img src="template/img/news1.png" alt="">
+                                            <div class="carousel-caption">
+                                                <h3>Отбеливание со<br>скидкой 20%</h3>
+                                                <p>С белоснежной улыбкой в отпуск!</p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <img src="template/img/news1.png" alt="">
+                                            <div class="carousel-caption">
+                                                <h3>Отбеливание со<br>скидкой 20%</h3>
+                                                <p>С белоснежной улыбкой в отпуск!</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main-advantages">
+        <div class="container main-advantages-items">
+            <div class="row main-advantages-row">
+                <div class="col-md-4 main-advantages-item">
+                    <div class="main-advantages-item-img">
+                        <img class="img-responsive" src="template/img/smile.svg" alt="">
+                    </div>
+                    <span class="main-advantages-item-descr">
+                        4115 счастливых<br>пациентов
+                    </span>
+                </div>
+                <div class="col-md-4 main-advantages-item">
+                    <div class="main-advantages-item-img">
+                        <img class="img-responsive" src="template/img/shield.svg" alt="">
+                    </div>
+                    <span class="main-advantages-item-descr">
+                        3205 спасенных<br>зуба
+                    </span>
+                </div>
+                <div class="col-md-4 main-advantages-item">
+                    <div class="main-advantages-item-img">
+                        <img class="img-responsive" src="template/img/8years.svg" alt="">
+                    </div>
+                    <span class="main-advantages-item-descr">
+                        9 лет успешной<br>работы
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?require("template/footer.html");?>
